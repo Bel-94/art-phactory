4cecda9f77315b9f49a94002891060d64d6b5050
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-05-27 02:20:02 +0300

    initial commit

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import Location, Image, Category
+from django.http  import HttpResponse, Http404
+import datetime as dt
 
 # Create your views here.
-def home(request):
-    return render(request, 'base.html')
+def welcome(request):
+    pass
+
+def all_images(request,image_id):
+    pass
+
+def image_location(request,location_name):
+    pass
+
+def search_cat(request):
+    pass
