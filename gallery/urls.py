e1b9b4b605946aae28b2a21ddf974b4b2e0da2ef
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-05-28 00:33:36 +0300

    add the search url

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,5 +2,6 @@ from django.urls import re_path, path
 from . import views
 
 urlpatterns = [
-    # re_path('', views.home,name='home'),
+    re_path('', views.welcome,name='Gallery'),
+    re_path(r'^search/', views.search_cat, name='search_category'),
 ]
