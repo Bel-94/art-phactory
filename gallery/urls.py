f93f940da645a1b36cccdcc07080e2403e199858
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-05-28 01:09:18 +0300

    add the url paths for image and location

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     re_path('', views.welcome,name='Gallery'),
     re_path(r'^search/', views.search_cat, name='search_category'),
+    re_path('^image/(?P<image_id>\d+)/$',views.all_images, name='image'),
+    re_path(r'^location/(?P<location_name>\w+)',views.image_location,name = 'location'),
 ]
