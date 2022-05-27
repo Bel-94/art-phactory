a6a34d38c7e8909ecdf096ee822a3b8bf6d6373c
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-05-27 22:00:37 +0300

    update the search function to search by location

--- gallery/views.py
+++ gallery/views.py
@@ -5,7 +5,10 @@ import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    pass
+    images = Image.objects.all()
+    location = Location.get_locations()
+
+    return render (request, 'index.html', {"images":images, "location":location})
 
 def all_images(request,image_id):
     pass
@@ -14,4 +17,12 @@ def image_location(request,location_name):
     pass
 
 def search_cat(request):
-    pass
+    location=Location.get_locations()
+
+    if 'category' in request.GET and request.GET["category"]:
+        category = request.GET.get("category")
+        search = Image.search_by_category(category)
+        message = f"{category}"
+        return render(request, 'search.html',{"message":message,"category": search,"location":location})
+    else:
+        return render(request, 'search.html')
